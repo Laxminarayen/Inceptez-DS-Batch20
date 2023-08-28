28d6b08f1348eb58705113425dd47664aeeea249
Laxminarayen <42375609+Laxminarayen@users.noreply.github.com>
2023-08-28 10:46:19 +0530

    Update 27-08

---
+++ 34. Flask-deployment/app.py
@@ -0,0 +1,29 @@
+# pip instal flask
+from flask import Flask, render_template, request
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load('pipe.pkl')
+
+# 1. Front end - Display the html files
+# 2. Back end - route the requests and put the values
+
+
+@app.route('/')
+def landing_page():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def prediction():
+    test_df = pd.DataFrame([request.form])
+    # print(test_df)
+    value = model.predict(test_df)[0]
+    value = str(round(value, 2))
+    return render_template('predict.html', price=value + "Lakhs")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
